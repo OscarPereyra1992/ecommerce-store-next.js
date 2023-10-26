b4539d9093ae1cec41958d2f9d99aae86795701f
OscarPereyra1992 <oscarpereyra.mhs@gmail.com>
2023-10-26 11:42:52 -0300

    Changes in database.

--- app/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -7,7 +7,7 @@ import Container from "@/components/ui/container";
 import Filter from "./components/filter";
 import NoResults from "@/components/ui/no-results";
 import ProductCard from "@/components/ui/product-card";
-
+import Navbar from "@/components/navbar";
 
 export const revalidate = 0;
 
@@ -15,28 +15,21 @@ interface CategoryPageProps {
   params: {
     categoryId: string;
   };
- 
 }
 
-const CategoryPage: React.FC<CategoryPageProps> = async ({
-  params,
-  
-}) => {
+const CategoryPage: React.FC<CategoryPageProps> = async ({ params }) => {
   const products = await getProducts({
     categoryId: params.categoryId,
-    
   });
 
-  const category = await getCategory(params.categoryId);
+ 
 
   return (
     <div className="bg-white">
+      <Navbar />
       <Container>
-        <Billboard data={category.billboard} />
         <div className="px-4 sm:px-6 lg:px-8 pb-24">
           <div className="lg:grid lg:grid-cols-6 lg:gap-x-8">
-            
-
             <div className="mt-6 lg:col-span-4 lg:mt-0">
               {products.length === 0 && <NoResults />}
               <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
@@ -44,11 +37,11 @@ const CategoryPage: React.FC<CategoryPageProps> = async ({
                   <ProductCard key={item.id} data={item} />
                 ))}
               </div>
+              
             </div>
           </div>
         </div>
       </Container>
-      Category
     </div>
   );
 };
