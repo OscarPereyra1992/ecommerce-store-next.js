f3b24e65252c5843ddbba6436afe0629dbe81209
OscarPereyra1992 <oscarpereyra.mhs@gmail.com>
2023-11-13 12:55:48 -0300

    Footer updated

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,3 +1,6 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const Footer = () =>{
     return(
         <footer >
@@ -8,12 +11,18 @@ const Footer = () =>{
           className="w-24 h-20 flex items-center justify-center"
         />
             <div className="mx-auto py-10">
+                
                 <p className="text-center text-xs text-white">
                     &copy; 2023 La Reina- Paraná.
                 </p>
-                <p className="text-center text-xs text-white">
+                <p className="text-center text-xs text-white items-center">
                      Página elaborada por Oscar Pereyra. All rights reserved.
                 </p>
+                <Link href="https://www.linkedin.com/in/oscar-pereyra-898baa128/"
+                target="_blank">
+            <Image className="flex mx-auto justify-center text-center items-center" src="/assets/linkedin-logo.png" alt="linkedin logo" width={20} height={20}/>
+                </Link>
+            
             </div>
       </div>
 
